6f547378824b4411d5780699328323a848e6e31a
Lucy Scarlet <yuiyukihira@pm.me>
2021-11-27 19:32:31 +0000

    Create Makepress Manager

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,90 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::collections::HashMap;
+
+use async_trait::async_trait;
+use futures::Stream;
+use thiserror::Error;
+
+use stream::{CreateMapper, DestroyMapper, GetMapper, StartMapper, StopMapper};
+
+mod stream;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("There is no instance called: `{0}`")]
+    InstanceMissing(String),
+    #[error("IO Error: `{0}`")]
+    IOError(#[from] std::io::Error),
+    #[error("Unknown error")]
+    Unknown,
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+pub enum Status {
+    Starting,
+    Running,
+    Failing,
+    Offline,
+}
+
+pub struct InstanceInfo {
+    pub name: String,
+    pub wordpress_status: Status,
+    pub database_status: Status,
+    pub created: i64,
+    pub labels: HashMap<String, String>,
+}
+
+pub struct CreateInfo {
+    pub name: String,
+    pub access_url: String,
+}
+
+#[async_trait]
+pub trait MakepressManager: Sized + Clone {
+    async fn get_all<T: AsRef<str>>(&self) -> Vec<Result<InstanceInfo>>;
+    async fn get<T: AsRef<str>>(&self, name: T) -> Result<InstanceInfo>;
+    async fn get_many<S>(&'static self, names: S) -> GetMapper<S, Self>
+    where
+        S: Stream + Send,
+        S::Item: AsRef<str>,
+    {
+        GetMapper::new(self, names)
+    }
+
+    async fn create<T: AsRef<str>>(&self, name: T) -> Result<InstanceInfo>;
+    async fn create_many<S>(&'static self, names: S) -> CreateMapper<S, Self>
+    where
+        S: Stream + Send,
+        S::Item: AsRef<str>,
+    {
+        CreateMapper::new(self, names)
+    }
+
+    async fn start<T: AsRef<str>>(&self, name: T) -> Result<InstanceInfo>;
+    async fn start_many<S>(&'static self, names: S) -> StartMapper<S, Self>
+    where
+        S: Stream + Send,
+        S::Item: AsRef<str>,
+    {
+        StartMapper::new(self, names)
+    }
+
+    async fn stop<T: AsRef<str>>(&self, name: T) -> Result<InstanceInfo>;
+    async fn stop_many<S>(&'static self, names: S) -> StopMapper<S, Self>
+    where
+        S: Stream + Send,
+        S::Item: AsRef<str>,
+    {
+        StopMapper::new(self, names)
+    }
+
+    async fn destroy<T: AsRef<str>>(&self, name: T) -> Result<InstanceInfo>;
+    async fn destroy_many<S>(&'static self, names: S) -> DestroyMapper<S, Self>
+    where
+        S: Stream + Send,
+        S::Item: AsRef<str>,
+    {
+        DestroyMapper::new(self, names)
     }
 }
---
+++ src/stream.rs
@@ -0,0 +1,271 @@
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use futures::{ready, Future, Stream};
+use pin_project_lite::pin_project;
+
+use crate::{InstanceInfo, MakepressManager};
+
+pin_project! {
+    #[must_use = "streams do nothing unless polled"]
+    pub struct GetMapper<St, M>
+    where St: Stream,
+    M: 'static
+    {
+        #[pin]
+        stream: St,
+        manager: &'static M,
+        #[pin]
+        pending_fut: Option<Pin<Box<dyn Future<Output = crate::Result<InstanceInfo>>>>>,
+    }
+}
+
+impl<St, M> GetMapper<St, M>
+where
+    St: Stream,
+    M: 'static,
+{
+    pub(crate) fn new(manager: &'static M, stream: St) -> Self {
+        Self {
+            stream,
+            manager,
+            pending_fut: None,
+        }
+    }
+}
+
+impl<St, M> Stream for GetMapper<St, M>
+where
+    St: Stream,
+    St::Item: AsRef<str> + 'static,
+    M: MakepressManager + 'static,
+{
+    type Item = crate::Result<InstanceInfo>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let mut this = self.project();
+        Poll::Ready(loop {
+            if let Some(fut) = this.pending_fut.as_mut().as_pin_mut() {
+                let res = ready!(fut.poll(cx));
+                this.pending_fut.set(None);
+                break Some(res);
+            } else if let Some(name) = ready!(this.stream.as_mut().poll_next(cx)) {
+                this.pending_fut.set(Some(this.manager.get(name)));
+            } else {
+                break None;
+            }
+        })
+    }
+}
+
+pin_project! {
+    #[must_use = "streams do nothing unless polled"]
+    pub struct CreateMapper<St, M>
+    where St: Stream,
+    M: 'static
+    {
+        #[pin]
+        stream: St,
+        manager: &'static M,
+        #[pin]
+        pending_fut: Option<Pin<Box<dyn Future<Output = crate::Result<InstanceInfo>>>>>,
+    }
+}
+
+impl<St, M> CreateMapper<St, M>
+where
+    St: Stream,
+    M: 'static,
+{
+    pub(crate) fn new(manager: &'static M, stream: St) -> Self {
+        Self {
+            stream,
+            manager,
+            pending_fut: None,
+        }
+    }
+}
+
+impl<St, M> Stream for CreateMapper<St, M>
+where
+    St: Stream,
+    St::Item: AsRef<str> + 'static,
+    M: MakepressManager + 'static,
+{
+    type Item = crate::Result<InstanceInfo>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let mut this = self.project();
+        Poll::Ready(loop {
+            if let Some(fut) = this.pending_fut.as_mut().as_pin_mut() {
+                let res = ready!(fut.poll(cx));
+                this.pending_fut.set(None);
+                break Some(res);
+            } else if let Some(name) = ready!(this.stream.as_mut().poll_next(cx)) {
+                this.pending_fut.set(Some(this.manager.create(name)));
+            } else {
+                break None;
+            }
+        })
+    }
+}
+
+pin_project! {
+    #[must_use = "streams do nothing unless polled"]
+    pub struct StartMapper<St, M>
+    where
+    St: Stream,
+    M: 'static,
+    {
+        #[pin]
+        stream: St,
+        manager: &'static M,
+        #[pin]
+        pending_fut: Option<Pin<Box<dyn Future<Output = crate::Result<InstanceInfo>>>>>,
+    }
+}
+
+impl<St, M> StartMapper<St, M>
+where
+    St: Stream,
+    M: 'static,
+{
+    pub(crate) fn new(manager: &'static M, stream: St) -> Self {
+        Self {
+            stream,
+            manager,
+            pending_fut: None,
+        }
+    }
+}
+
+impl<St, M> Stream for StartMapper<St, M>
+where
+    St: Stream,
+    St::Item: AsRef<str> + 'static,
+    M: MakepressManager + 'static,
+{
+    type Item = crate::Result<InstanceInfo>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let mut this = self.project();
+        Poll::Ready(loop {
+            if let Some(fut) = this.pending_fut.as_mut().as_pin_mut() {
+                let res = ready!(fut.poll(cx));
+                this.pending_fut.set(None);
+                break Some(res);
+            } else if let Some(name) = ready!(this.stream.as_mut().poll_next(cx)) {
+                this.pending_fut.set(Some(this.manager.start(name)));
+            } else {
+                break None;
+            }
+        })
+    }
+}
+
+pin_project! {
+    #[must_use = "streams do nothing unless polled"]
+    pub struct StopMapper<St, M>
+    where
+    St: Stream,
+    M: 'static
+    {
+        #[pin]
+        stream: St,
+        manager: &'static M,
+        #[pin]
+        pending_fut: Option<Pin<Box<dyn Future<Output = crate::Result<InstanceInfo>>>>>,
+    }
+}
+
+impl<St, M> StopMapper<St, M>
+where
+    St: Stream,
+    M: 'static,
+{
+    pub(crate) fn new(manager: &'static M, stream: St) -> Self {
+        Self {
+            stream,
+            manager,
+            pending_fut: None,
+        }
+    }
+}
+
+impl<St, M> Stream for StopMapper<St, M>
+where
+    St: Stream,
+    St::Item: AsRef<str> + 'static,
+    M: MakepressManager + 'static,
+{
+    type Item = crate::Result<InstanceInfo>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let mut this = self.project();
+        Poll::Ready(loop {
+            if let Some(fut) = this.pending_fut.as_mut().as_pin_mut() {
+                let res = ready!(fut.poll(cx));
+                this.pending_fut.set(None);
+                break Some(res);
+            } else if let Some(name) = ready!(this.stream.as_mut().poll_next(cx)) {
+                this.pending_fut.set(Some(this.manager.stop(name)));
+            } else {
+                break None;
+            }
+        })
+    }
+}
+
+pin_project! {
+    #[must_use = "streams do nothing unless polled"]
+    pub struct DestroyMapper<St, M>
+    where St: Stream,
+    M: 'static
+    {
+        #[pin]
+        stream: St,
+        manager: &'static M,
+        #[pin]
+        pending_fut: Option<Pin<Box<dyn Future<Output = crate::Result<InstanceInfo>>>>>,
+    }
+}
+
+impl<St, M> DestroyMapper<St, M>
+where
+    St: Stream,
+    M: 'static,
+{
+    pub(crate) fn new(manager: &'static M, stream: St) -> Self {
+        Self {
+            stream,
+            manager,
+            pending_fut: None,
+        }
+    }
+}
+
+impl<St, M> Stream for DestroyMapper<St, M>
+where
+    St: Stream,
+    St::Item: AsRef<str> + 'static,
+    M: MakepressManager + 'static,
+{
+    type Item = crate::Result<InstanceInfo>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let mut this = self.project();
+        Poll::Ready(loop {
+            if let Some(fut) = this.pending_fut.as_mut().as_pin_mut() {
+                let res = ready!(fut.poll(cx));
+                this.pending_fut.set(None);
+                break Some(res);
+            } else if let Some(name) = ready!(this.stream.as_mut().poll_next(cx)) {
+                this.pending_fut.set(Some(this.manager.destroy(name)));
+            } else {
+                break None;
+            }
+        })
+    }
+}
